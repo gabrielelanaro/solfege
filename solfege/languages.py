c770956d03842dcbabbb4b8f0ceb5bc5fcda1e7b
Gabriele Lanaro <gabriele.lanaro@gmail.com>
2011-01-31 21:17:57 +0100

    added languages.py needed to run

---
+++ solfege/languages.py
@@ -0,0 +1,28 @@
+# Generated at build time by tools/buildscript.py
+# Do not edit. Changes will be lost.
+C_locale_idx = 1
+languages = [
+  'system default',
+  'English/United States [en-us]',
+   'eo',
+   'sv',
+   'es',
+   'fa',
+   'nl',
+   'vi',
+   'cs',
+   'ru',
+   'nb',
+   'pt_BR',
+   'gl',
+   'hu',
+   'fi',
+   'tr',
+   'zh_CN',
+   'fr',
+   'et',
+   'de',
+   'it',
+   'pl',
+   'da',
+]
